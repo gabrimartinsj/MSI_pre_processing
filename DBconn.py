f39ef51a50ced3fff41d91bca4947173726ce352
Gabrimartins <gabrimartinsjuarez@gmail.com>
2023-11-02 20:46:08 -0300

    Captura as msg da janela em uma lista e concatena num df de msgs únicas.

--- DBconn.py
+++ DBconn.py
@@ -1,5 +1,6 @@
 import psycopg2
 import re
+import pandas as pd
 
 # Parâmetros de conexão ao banco de dados
 db_params = {
@@ -10,6 +11,9 @@ db_params = {
 }
 
 url_pattern = r'(https?://\S+)'
+
+# Crie uma lista para armazenar as linhas capturadas
+new_rows = []
 
 try:
     # Conectando ao banco de dados
@@ -24,7 +28,6 @@ try:
 
     while True:
         cursor.execute("SELECT * FROM messages_order_by_channel_utc_limited LIMIT %s OFFSET %s", (page_size, offset))
-
         results = cursor.fetchall()
 
         if not results:
@@ -36,12 +39,16 @@ try:
                 print("Mensagem com URL:")
                 print(row[2], row[3], row[4])
                 print("\n")
+                # Adicione a linha capturada à lista de linhas
+                new_rows.append([row[2], row[3], row[4]])
 
                 # Imprime as 5 linhas anteriores
                 print("5 linhas anteriores:")
                 for i in range(max(0, results.index(row) - 5), results.index(row)):
                     if results[i][2] == row[2]:
                         print(results[i][2], results[i][3], results[i][4])
+                        # Adicione a linha capturada à lista de linhas
+                        new_rows.append([results[i][2], results[i][3], results[i][4]])
                 print("\n")
 
                 # Imprime as 20 linhas seguintes
@@ -49,13 +56,21 @@ try:
                 for i in range(results.index(row) + 1, min(results.index(row) + 21, len(results))):
                     if results[i][2] == row[2]:
                         print(results[i][2], results[i][3], results[i][4])
+                        # Adicione a linha capturada à lista de linhas
+                        new_rows.append([results[i][2], results[i][3], results[i][4]])
                 print("\n")
 
                 # Encerra o loop após encontrar um conjunto
                 break
-        break
+
         offset += page_size
 
+    # Crie um DataFrame a partir da lista de linhas
+    df = pd.DataFrame(new_rows, columns=["channel_id", "message_data", "message_utc"])
+
+    # Remova as duplicatas do DataFrame, se necessário
+    df = df.drop_duplicates()
+    print(df)
 
     # Fechando o cursor
     cursor.close()
